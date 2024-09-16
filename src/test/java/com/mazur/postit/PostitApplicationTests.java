9fb4ce801ce01ba2caedbfc00e80c72beb603851
Mateusz Mazur <matthew@onet.pl>
2024-09-16 20:30:15 +0200

    init

---
+++ src/test/java/com/mazur/postit/PostitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazur.postit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
