43a282dcd7505549c3b214c4e30074882ab0010b
Mateusz Mazur <matthew@onet.pl>
2024-09-17 23:18:19 +0200

    added content trim

--- src/main/java/com/mazur/postit/mapper/NoteMapper.java
+++ src/main/java/com/mazur/postit/mapper/NoteMapper.java
@@ -4,18 +4,27 @@ import com.mazur.postit.db.entity.NoteEntity;
 import com.mazur.postit.dto.InputNoteDto;
 import com.mazur.postit.dto.OutputNoteDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 import java.util.Collection;
 import java.util.List;
 
 @Mapper
 public interface NoteMapper {
-    NoteEntity mapToEntity(InputNoteDto dto);
+    @Named("trim")
+    static String trim(final String value) {
+        return value != null ? value.trim() : null;
+    }
 
     OutputNoteDto mapToDto(NoteEntity entity);
 
     List<OutputNoteDto> mapToDto(Collection<NoteEntity> entities);
 
+    @Mapping(target = "content", qualifiedByName = "trim")
+    NoteEntity mapToEntity(InputNoteDto dto);
+
+    @Mapping(target = "content", qualifiedByName = "trim")
     void updateFromDto(InputNoteDto dto, @MappingTarget NoteEntity entity);
 }
